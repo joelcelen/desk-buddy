98785109be2476fe0ade1cd52434eeeb02a94adc
joel <joelcel@student.chalmers.se>
2023-04-17 11:28:02 +0200

    Read temperature and humidity form sensor
    This commit adds code to read the humidity and temperature from the Grove DIT11 sensor and display it in the serial montior.

--- arduino_src/arduino_src.ino
+++ arduino_src/arduino_src.ino
@@ -1,9 +1,42 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "DHT.h" // DHT11 humidity/temperature sensor
+#include "TFT_eSPI.h" // Wio terminal screen
 
+// Create an instance of DHT
+DHT dht(A0, DHT11);
+
+// initialize TFT LCD
+TFT_eSPI screen;
+
+// Define the serial
+#define serial Serial
+
+void setup() {
+    serial.begin(115200);
+    serial.println("DHT11 test!");
+    Wire.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  
+  // Humidity sensor DT11 -----------------------------------
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
 
+  float temp_hum_val[2] = {0};
+  
+  if (!dht.readTempAndHumidity(temp_hum_val)) {
+    serial.print("Humidity: ");
+    serial.print(temp_hum_val[0]);
+    serial.print(" %\t");
+    serial.print("Temperature: ");
+    serial.print(temp_hum_val[1]);
+    serial.println(" *C");
+  } else {
+    serial.println("Failed to get temprature and humidity value.");
+  }
+  
+  //--------------------------------------------------------------
+
+  delay(1000);
 }
