9516780be80b31079f3e8140860bbc88e82c587e
Malte Bengtsson <bmalte@student.chalmers.se>
2023-04-18 15:53:54 +0200

    Read input from user via button press
    This commit implements input recognition via internal button on Wio and outputs message on device screen.

--- arduino_src/arduino_src.ino
+++ arduino_src/arduino_src.ino
@@ -1,10 +1,12 @@
 #include "DHT.h" // DHT11 humidity/temperature sensor
 #include "TFT_eSPI.h" // Wio terminal screen
+#include "button.h" //Reference to the header file for button
 
 // Create an instance of DHT
 DHT dht(A0, DHT11);
 
-
+//Create instance of button class named "button"
+button button;
 
 // initialize TFT LCD
 TFT_eSPI screen;
@@ -13,11 +15,17 @@ void setup() {
   
   Serial.begin(9600);
   Wire.begin();
+
+  pinMode(BUTTON_1, INPUT_PULLUP); //Sets the mode of "BUTTON_1" pin to input with an internal pull-up resistor.
+  button.begin();
 }
 
 void loop() {
+
+  //Button --------------------------------------------------  
   
-  
+  button.checkButton();
+
   // Humidity sensor DT11 -----------------------------------
 
   float temperature = dht.readTemperature();
@@ -33,13 +41,13 @@ void loop() {
 
   // Light sensor--------------------------------------------
 
-  lightValue = analogRead(lightPin);  // Read the analog value from the light sensor
+  int lightValue = analogRead(A2);  // Read the analog value from the light sensor, assigned to pin A2
 
-  // Printing the results in the Serial monitor  
+  //Printing the results in the Serial monitor  
   Serial.print("Light sensor value: ");  // Print the sensor value to the serial monitor
   Serial.println(lightValue);  
 
   //--------------------------------------------------------------
-
-  delay(1000);
+  
+  delay(100);
 }
