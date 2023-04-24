5702426a99f75d2d74b45b507b3768c5d805ba1e
Nasit Vurgun <gusvurna@student.gu.se>
2023-04-24 16:16:33 +0200

    Publish sensor data to secure MQTT broker
    This commit implements publishing of temperature, humidity, and light sensor data to the private MQTT broker (using SSL encryption) every 30 seconds.

--- arduino_src/arduino_src.ino
+++ arduino_src/arduino_src.ino
@@ -37,7 +37,7 @@ const char* mqtt_pub_light = TOPIC_PUB_LIGHT; // publish light reading
 
 WiFiClientSecure wifiSSLClient; //wifi client for PubSub with SSL/TLS cryptographic protocols
 PubSubClient mqttClient(wifiSSLClient); //MQTT client with SSL/TLS communication protocols
-
+long lastMsg = 0; //last message time
 
 // Create an instance of DHT
 DHT dht(A0, DHT11);
@@ -51,6 +51,7 @@ TFT_eSPI screen;
 void setup() {
   
   Serial.begin(9600);
+  while (!Serial);
   Wire.begin();
 
   //wifi_setup
@@ -72,8 +73,12 @@ void setup() {
 }
 
 void loop() {
+  //mqtt_loop - checks for connection to MQTT broker, listens to topic deskBuddy for now
+  if (!mqttClient.connected()) {
+    mqttReconnect();
+  }
+
   //Button --------------------------------------------------  
-  
   button.checkButton();
 
   // Humidity sensor DT11 -----------------------------------
@@ -99,30 +104,64 @@ void loop() {
 
   //--------------------------------------------------------------
   
-  //mqtt_loop - checks for connection to MQTT broker, listens to topic deskBuddy for now
-  if (!mqttClient.connected()) {
-    Serial.println("Connecting to MQTT broker...");
-    if (mqttClient.connect(mqtt_client, mqtt_username, mqtt_password)) {
-      Serial.println("Connected to MQTT broker!");
-      mqttClient.subscribe("deskBuddy");
-      mqttClient.publish("deskBuddy", "hello from Wio deskBuddy!"); //mqtt_sub
-      mqttClient.publish("deskBuddy", String(lightValue).c_str()); //mqtt_pub_light
-      mqttClient.publish("deskBuddy", String(humidity).c_str()); //mqtt_pub_humid
-      mqttClient.publish("deskBuddy", String(temperature).c_str()); //mqtt_pub_temp
-    } else {
-      Serial.println("Connection to MQTT broker failed!");
-    }
-  }
-  //mqttClient.loop();
+  // Sending data every 30 seconds
+  long now = millis();
+  if (now - lastMsg > 30000) {
+    lastMsg = now;
 
-  delay(5000);
+  lightValue = analogRead(A2);
+  temperature = dht.readTemperature();
+  humidity = dht.readHumidity();
+  String data="{\"light level\": "+String(lightValue)+","+"\"temperature level\": "+String(temperature)+","+"\"humidity level\": "+String(humidity)+"}";
+
+  if (!mqttClient.publish(mqtt_pub_deskBuddy, data.c_str())) {
+    Serial.println("Message failed to send.");
+  }
+  Serial.printf("Message Send [%s] ", mqtt_pub_deskBuddy);
+  Serial.println(data);
+  }
+  
+  mqttClient.loop(); // Receive messages on subscribed topics and trigger mqttCallback function
+  delay(10000);
 }
 
-// mqtt_callback
-void mqttCallback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message received: ");
-  for (int i = 0; i < length; i++) {
+void mqttCallback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++)
+  {
     Serial.print((char)payload[i]);
   }
   Serial.println();
+}
+
+void mqttReconnect()
+{
+  // Loop until we're reconnected
+  while (!mqttClient.connected())
+  {
+    Serial.println("Connecting to MQTT broker...");
+    // Attempt to connect
+    if (mqttClient.connect(mqtt_client, mqtt_username, mqtt_password)) {
+      // Once connected, publish an announcement...
+      Serial.println("Connected to MQTT broker!");
+      mqttClient.publish(mqtt_pub_deskBuddy, "{\"message\": \"Wio Terminal is connected!\"}");
+      Serial.println("Published connection message successfully!");
+      // ... and resubscribe
+      Serial.print("Subcribed to: ");
+      mqttClient.subscribe(mqtt_pub_deskBuddy);
+      //mqttClient.subscribe(mqtt_pub_light);
+      //mqttClient.subscribe(mqtt_pub_humid);
+      //mqttClient.subscribe(mqtt_pub_temp);
+    } else {
+      Serial.println("Connection to MQTT broker failed!");
+      Serial.print("failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
 }
