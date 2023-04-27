5cb482f4f00568349667499b74c6ad6294853f80
joel <joelcel@student.chalmers.se>
2023-04-27 15:02:09 +0200

    Add name attribute to room profile class
    This commit adds a name attribute to the room, the default constructor names the room Default

--- deskBuddyApp/app/src/main/java/com/example/deskbuddyapp/RoomProfile.java
+++ deskBuddyApp/app/src/main/java/com/example/deskbuddyapp/RoomProfile.java
@@ -4,12 +4,15 @@ package com.example.deskbuddyapp;
 /** Class for saving user preferences. **/
 
 public class RoomProfile {
+
+    private String roomName;
     private double temperature;
     private double humidity;
     private double lightLevel;
 
     // Constructor with specific values
-    public RoomProfile(double temperature, double humidity, double lightLevel){
+    public RoomProfile(String roomName, double temperature, double humidity, double lightLevel){
+        this.roomName = roomName;
         this.temperature = temperature;
         this.humidity = humidity;
         this.lightLevel = lightLevel;
@@ -17,12 +20,17 @@ public class RoomProfile {
 
     // Constructor with default values.
     public RoomProfile(){
+        this.roomName = "Default";
         this.temperature = 23.0;
         this.humidity = 40.0;
         this.lightLevel = 700;
     }
 
     // Getters and setters.
+    public String getRoomName() {
+        return roomName;
+    }
+
     public double getTemperature() {
         return temperature;
     }
@@ -33,6 +41,10 @@ public class RoomProfile {
 
     public double getLightLevel() {
         return lightLevel;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
     }
 
     public void setTemperature(double temperature) {
