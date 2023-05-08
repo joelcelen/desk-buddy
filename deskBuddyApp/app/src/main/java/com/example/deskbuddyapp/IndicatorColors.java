530b9b4b997a5dc3371aa3f05eb4171350b6d093
Karl Eriksson <kaeriks@student.chalmers.se>
2023-05-08 15:53:42 +0200

    Refactor enum class for wio colors to display
    this commit refactors the name of the enum class to be clearer, and for the colors to be Strings with a getter method in the class

--- deskBuddyApp/app/src/main/java/com/example/deskbuddyapp/IndicatorColors.java
+++
@@ -1,8 +0,0 @@
-package com.example.deskbuddyapp;
-
-//enums for the colors that will be published to the Wio terminal from the app
-public enum IndicatorColors {
-    TFT_RED,
-    TFT_ORANGE,
-    TFT_GREEN
-}
