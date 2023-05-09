5b7e2e7e15451c2d37ad83b78bc251d1c71148d5
joel <joelcel@student.chalmers.se>
2023-05-09 19:40:14 +0200

    Test classes for the Profile activity
    This commit adds two tester classes related to the ProfileActivity. The RoomProfileTest is a unit test class that tests the creation of new RoomProfiles, the ProfileActivityTest is a UI test class.

---
+++ deskBuddyApp/app/src/test/java/com/example/deskbuddyapp/RoomProfileTest.java
@@ -0,0 +1,26 @@
+package com.example.deskbuddyapp;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class RoomProfileTest {
+
+    @Test
+    public void createDefaultProfile(){
+        RoomProfile profile = new RoomProfile();
+        assertEquals("Default", profile.getRoomName());
+        assertEquals(23.0, profile.getTemperature(), 0.1);
+        assertEquals(40.0, profile.getHumidity(), 0.1);
+        assertEquals(700.0, profile.getLightLevel(), 0.1);
+    }
+
+    @Test
+    public void createCustomProfile(){
+        RoomProfile profile = new RoomProfile("Custom", 35.0, 60.0, 1000.0);
+        assertEquals("Custom", profile.getRoomName());
+        assertEquals(35.0, profile.getTemperature(), 0.1);
+        assertEquals(60.0, profile.getHumidity(), 0.1);
+        assertEquals(1000.0, profile.getLightLevel(), 0.1);
+    }
+
+}
